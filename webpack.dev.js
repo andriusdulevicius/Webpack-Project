9b0b48a7988643849ae9a0a8891c6b70fa4936bd
AndriusDulevicius <andriusdulevicius@email.com>
2021-05-24 11:51:06 +0300

    installed html generation plugins

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,4 +1,5 @@
 const path = require('path'); // node modulis dirbti su keliais iki failu
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //html generavimo pluginas.
 
 module.exports = {
     mode: 'development',
@@ -8,5 +9,9 @@ module.exports = {
     },
     output: {},
     module: {},
-    plugins: [],
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/html/template.html',
+        }),
+    ],
 };
